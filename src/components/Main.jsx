3dca571cfb3017d0e091adbe28abc88558592000
Hamza Hayd <dado.org@hotmail.com>
2021-01-15 18:52:22 +0100

    install som axtra packagest

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ const Main = () => {
     return (
         <div>
             <h2>hello from Main-component</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium numquam dignissimos sint in sed optio atque eius? Cum, saepe.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cumque accusantium numquam dignissimos sint
+                in sed optio atque eius? Cum, saepe.</p>
         </div>
     )
 }
