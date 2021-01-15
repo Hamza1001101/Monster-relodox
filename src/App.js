eabbc23e94a574691611c71dfb362ab94bb9e1ff
Hamza Hayd <dado.org@hotmail.com>
2021-01-15 18:36:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Main from "./components/Main"
+import React, { Component } from 'react'
 
-const App = () => {
-    return (
+class App extends Component {
+
+    render() {
+ return (
         <div>
             <h1>Welcome Home</h1>
+            <Main />
         </div>
     )
+
+    }
+   
 }
 
 export default App
