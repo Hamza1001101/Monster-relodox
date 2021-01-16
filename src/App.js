62426a467f0f8293635d414f0796cebff20c5060
Hamza Hayd <dado.org@hotmail.com>
2021-01-16 01:33:37 +0100

    added api calls

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import React, { Component } from 'react'
 
 class App extends Component {
 
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            users: [],
+            
+        }
+    }
+    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(resp => resp.json())
+        .then(users => this.setState({users}))
+    }
+    
     render() {
        return (
         <div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
 
 const Main = () => {
+
     const [counter, setCounter] = useState(0)
+
+    const [decreaser, setDecreaser] = useState(0)
+    
+    function decreaseHandler() {
+        if(decreaser < 0) return
+        setDecreaser(decreaser-1)
+    }
+
 
     const handleClick = () => {
         if(counter === 10) return
@@ -14,6 +23,7 @@ const Main = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Cumque accusantium numquam dignissimos sint
                 in sed optio atque eius? Cum, saepe.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium, amet possimus placeat hic enim reprehenderit dicta, quia officia nisi saepe quasi odio suscipit quo. Ipsa, minima! Dolores in atque perspiciatis delectus neque itaque facilis debitis consequatur! Nemo?</p>
             <h1> {counter} </h1>
             <button onClick={handleClick} style={{cursor: 'pointer'}}>  Add + </button>
         </div>
