98bbbbb7ddff635b5fc3207bc51c745cb6ffe114
Hamza Hayd <dado.org@hotmail.com>
2021-01-16 12:34:31 +0100

    Nöjd med insatsen

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
-
-import React, { Component } from 'react'
-import Main from "./components/Main"
-import Template from './components/Template'
+import React, { Component } from "react";
+import Main from "./components/Main";
+import Template from "./components/Template";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            users: [],
-        }
-    }
-    
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(resp => resp.json())
-            .then(users => this.setState({users: users}) )
-    }
-    read = () =>  {
-        console.log(this.state.users)
-    }
-    
-    render() {
-        const { users } = this.state
-        const listRender = users.map(({id, name, username, email}) => {
-            return <Template key={id} name={name} username={username} email={email} />
-        })
-       return (
-        <div>
-            <h1>Welcome Home</h1>
-               <Main />
-              
-                    {listRender}
-               </div>
-              
-        
-    )
+    this.state = {
+      users: [],
+      userinput: ''
+    };
+  }
 
-    }
-   
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((users) => this.setState({ users: users }));
+  }
+  read = () => {
+    console.log(this.state.users);
+  };
+
+  userHandler = e => {
+    this.setState({
+      userinput: e.target.value
+    })
+  }
+  
+  render() {
+    const { users } = this.state;
+    const listRender = users.map(({ id, name, username, email }) => {
+      return (
+        <Template key={id} name={name}
+          username={username}
+          email={email}
+        />
+      );
+    });
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+        <Main />
+
+        {listRender}
+        <form>
+          <input type='text' placeholder='Add Task' name='userinput' onChange={this.userHandler} />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,35 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Main = () => {
+  const [counter, setCounter] = useState(0);
 
-    const [counter, setCounter] = useState(0)
+  const [decreaser, setDecreaser] = useState(10);
 
-    const [decreaser, setDecreaser] = useState(10)
+  function decreaseHandler() {
+    if (decreaser <= 0) return;
+    setDecreaser(decreaser - 1);
+  }
+
+  const handleClick = () => { 
+    if (counter === 10) return;
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <h2>hello from Main-component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+        accusantium numquam dignissimos sint in sed optio atque eius? Cum,
+        saepe.
+      </p>
+
+      <h1> {counter} </h1>
+      <h1> {decreaser} </h1>
+      <button onClick={handleClick} style={{ cursor: "pointer" }}>
     
-    function decreaseHandler() {
-        if(decreaser <= 0) return
-        setDecreaser(decreaser-1)
-    }
-
-
-    const handleClick = () => {
-        if(counter === 10) return
-        setCounter(counter+1)
-    }
-
-    return (
-        <div>
-            <h2>hello from Main-component</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Cumque accusantium numquam dignissimos sint
-                in sed optio atque eius? Cum, saepe.</p>
-           
-            <h1> {counter} </h1>
-            <h1> {decreaser} </h1>
-            <button onClick={handleClick} style={{ cursor: 'pointer'}}>  Add + </button>
-            <button onClick={decreaseHandler} style={{ cursor: 'pointer' }} >Substract - </button>
-            
-        </div>
-    )
-}
-export default Main
+        Add +
+      </button>
+      <button
+        style={{
+          color: "black",
+          cursor: "pointer",
+          backgroundColor: "white",
+        }}
+        onClick={decreaseHandler}
+      >
+        Substract -
+      </button>
+    </div>
+  );
+};
+export default Main;
