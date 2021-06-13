7794f42060728bd8c77f001832f45e2210458363
Hamza Hayd <dado.org@hotmail.com>
2021-06-13 18:00:24 +0200

    Updated the entire App to Hooks

--- src/App1.js
+++ src/App1.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Searchbar from "./components/Searchbar";
-import Card from './components/Card'
+import Card from "./components/Card";
 
 const App1 = () => {
-    const [users, setUser] = useState([]);
-    const [userinput, setUserinput] = useState("");
+  const [users, setUser] = useState([]);
+  const [userinput, setUserinput] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -13,15 +13,15 @@ const App1 = () => {
       .then((user) => setUser(user));
   }, []);
 
-    const userHandler = (e) => {
-      setUserinput(e.target.value);
-    };
-    const filteredList = users.filter((user) => {
-        return (
-          user.name.toLowerCase().includes(userinput.toLowerCase())
-      )
-    }).map(({ id, name, username, email }) => (
-        <Card key={id} name={name} username={username} email={email} />
+  const userHandler = (e) => {
+    setUserinput(e.target.value);
+  };
+  const filteredList = users
+    .filter((user) => {
+      return user.name.toLowerCase().includes(userinput.toLowerCase());
+    })
+    .map(({ id, name, username, email }) => (
+      <Card id={id} key={id} name={name} username={username} email={email} />
     ));
   return (
     <div>
